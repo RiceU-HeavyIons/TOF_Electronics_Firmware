389fd9bd230714d3d0c0d7d27a0beeee4c1db14e
J Schambach <jschamba@physics.utexas.edu>
2004-12-03 23:40:13 +0000

    This commit was manufactured by cvs2svn to create tag 'TDIG_MCU_BNL_11-23-04r'.

--- TDIG/PLD/dff_sclr_sset_inst.vhd
+++
@@ -1,7 +0,0 @@
-dff_sclr_sset_inst : dff_sclr_sset PORT MAP (
-		clock	 => clock_sig,
-		sclr	 => sclr_sig,
-		sset	 => sset_sig,
-		data	 => data_sig,
-		q	 => q_sig
-	);
--- TDIG/PLD/shiftreg_inst.vhd
+++
@@ -1,5 +0,0 @@
-SHIFTREG_inst : SHIFTREG PORT MAP (
-		clock	 => clock_sig,
-		shiftin	 => shiftin_sig,
-		shiftout	 => shiftout_sig
-	);
