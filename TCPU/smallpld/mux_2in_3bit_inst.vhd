4e014cf2417304213847dd7e0ca56f2502d0d948
J Schambach <jschamba@physics.utexas.edu>
2004-12-03 21:05:27 +0000

    TCPU small PLD firmware installed at BNL on 11/23/04

---
+++ TCPU/smallpld/mux_2in_3bit_inst.vhd
@@ -0,0 +1,6 @@
+mux_2in_3bit_inst : mux_2in_3bit PORT MAP (
+		data1x	 => data1x_sig,
+		data0x	 => data0x_sig,
+		sel	 => sel_sig,
+		result	 => result_sig
+	);
---
+++ TCPU/smallpld/mux_2in_4bit_inst.vhd
@@ -0,0 +1,6 @@
+mux_2in_4bit_inst : mux_2in_4bit PORT MAP (
+		data1x	 => data1x_sig,
+		data0x	 => data0x_sig,
+		sel	 => sel_sig,
+		result	 => result_sig
+	);
