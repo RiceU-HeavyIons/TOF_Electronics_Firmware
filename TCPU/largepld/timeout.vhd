e6ec75aa8a7a8eb3e1750d04cf2d3ed228502bc9
J Schambach <jschamba@physics.utexas.edu>
2006-03-27 19:31:27 +0000

    Make counter width a GENERIC parameter, so it can be used with different timeout values

--- TCPU/largepld/timeout.vhd
+++ TCPU/largepld/timeout.vhd
@@ -1,51 +1,60 @@
-    LIBRARY ieee; USE ieee.std_logic_1164.all;        
-    LIBRARY lpm; USE lpm.lpm_components.all;
-    use work.tcpu_package.all;
-    
-    entity timeout is 
-    		port(clk, reset : in std_logic;
-    			clr_timeout : in std_logic;
-    			timeout_valid : out std_logic  		
-    		);
-    end timeout;
-    
-    architecture lwb1 of timeout is
- 		
-		signal counter_reset, term_cnt, term_cnt_flag, count_enable, stop_count : std_logic;
-		signal cnt_val : std_logic_vector(9 downto 0);
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+LIBRARY lpm;
+USE lpm.lpm_components.ALL;
+USE work.tcpu_package.ALL;
 
-    begin
-    		counter_reset <= reset or clr_timeout;
-    		stop_count <= reset or term_cnt;
-    		
-    
-    		timer : timeout_ctr PORT MAP (
-			clock	 => clk,
-			cnt_en	 => '1',        --count_enable,
-			sclr	 => counter_reset,
-			q	 => cnt_val,
-			cout	 => term_cnt);
-    
-    		-- this flag is set when counter reaches terminal cnt
-    		timeout_flag : DFF_sclr_sset PORT MAP (
-			clock	 => clk,
-			sclr	 => counter_reset,
-			sset	 => term_cnt,
-			data	 => term_cnt_flag,
-			q	 => term_cnt_flag);
-		
-		timeout_valid <= term_cnt_flag;
- 
- 		-- this ff enables counter after 'clr_timeout' input
+
+ENTITY timeout IS
+  GENERIC (
+    nbit : integer := 10);              -- width of counter
+  PORT(clk, reset    : IN  std_logic;
+       clr_timeout   : IN  std_logic;
+       timeout_valid : OUT std_logic
+       );
+END timeout;
+
+ARCHITECTURE lwb1 OF timeout IS
+  
+  SIGNAL counter_reset, term_cnt   : std_logic;
+  SIGNAL term_cnt_flag, stop_count : std_logic;
+  SIGNAL cnt_val                   : std_logic_vector(nbit-1 DOWNTO 0);
+
+BEGIN
+  counter_reset <= reset OR clr_timeout;
+  stop_count    <= reset OR term_cnt;
+
+  timer : lpm_counter
+    GENERIC MAP (
+      lpm_width     => nbit,
+      lpm_type      => "LPM_COUNTER",
+      lpm_direction => "UP")
+    PORT MAP (
+      sclr  => counter_reset,
+      clock => clk,
+      cout  => term_cnt,
+      q     => cnt_val);
+
+  -- this flag is set when counter reaches terminal cnt
+  timeout_flag : DFF_sclr_sset PORT MAP (
+    clock => clk,
+    sclr  => counter_reset,
+    sset  => term_cnt,
+    data  => term_cnt_flag,
+    q     => term_cnt_flag);
+
+  timeout_valid <= term_cnt_flag;
+
+  -- this ff enables counter after 'clr_timeout' input
 --              enable_ff : DFF_sclr_sset PORT MAP (
---			clock	 => clk,
---			sclr	 => stop_count,
---			sset	 => clr_timeout,
---			data	 => count_enable,
---                	q	 => count_enable);
+--                      clock    => clk,
+--                      sclr     => stop_count,
+--                      sset     => clr_timeout,
+--                      data     => count_enable,
+--                      q        => count_enable);
 
-    end lwb1;
-    
-    
-    
-    
+END lwb1;
+
+
+
+
