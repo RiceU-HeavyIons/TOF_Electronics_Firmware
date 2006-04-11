70bcb568f7c5c1d8245a8d9f30c70f3678c14d3e
J Schambach <jschamba@physics.utexas.edu>
2006-04-11 22:15:58 +0000

    New method of defining configuration bits

--- TDIG/MCU/TDIG-B.asm
+++ TDIG/MCU/TDIG-B.asm
@@ -3,18 +3,18 @@
 ; file: TDIG-B.asm
 
 ;******************************************************************************
-;    Date:          9/13/04                                                  *
+;    Date:          9/13/04                                                   *
 ;    File Version:  2.0                                                       *
 ;                                                                             *
-;    Author:		Justin Kennington          								  *
-;    Company:       Blue Sky Electronics           						      *
+;    Author:		Justin Kennington                                     *
+;    Company:       Blue Sky Electronics                                      *
 ;                                                                             * 
 ;******************************************************************************
 
 ;******************************************************************************
 ;                                                                             *
-;    Files required:         P18F8720.INC   								  *
-;                            18f8720.lkr								      *
+;    Files required:         P18F8720.INC                                     *
+;                            18f8720.lkr                                      *
 ;                            MCU_config.asm                                   *
 ;                                                                             *
 ;******************************************************************************
@@ -29,22 +29,38 @@
 
 ;******************************************************************************
 ; Configuration bits
+	;;  This is the old way of defining the configuration bits:
+		
 ; The __CONFIG directive defines configuration data within the .ASM file.
 ; The labels following the directive are defined in the P18F8720.INC file.
 ; The PIC18FXX20 Data Sheet explains the functions of the configuration bits.
 
-	__CONFIG	_CONFIG1H, _OSCS_OFF_1H & _ECIO_OSC_1H		
-	__CONFIG	_CONFIG2L, _BOR_OFF_2L & _BORV_20_2L & _PWRT_OFF_2L
-	__CONFIG	_CONFIG2H, _WDT_OFF_2H & _WDTPS_128_2H
-	__CONFIG	_CONFIG3L, _XMC_MODE_3L
-	__CONFIG	_CONFIG3H, _CCP2MX_ON_3H
-	__CONFIG	_CONFIG4L, _STVR_OFF_4L & _LVP_OFF_4L & _DEBUG_ON_4L
-	__CONFIG	_CONFIG5L, _CP0_OFF_5L & _CP1_OFF_5L & _CP2_OFF_5L & _CP3_OFF_5L & _CP4_OFF_5L & _CP5_OFF_5L & _CP6_OFF_5L & _CP7_OFF_5L 
-	__CONFIG	_CONFIG5H, _CPB_OFF_5H & _CPD_OFF_5H
-	__CONFIG	_CONFIG6L, _WRT0_OFF_6L & _WRT1_OFF_6L & _WRT2_OFF_6L & _WRT3_OFF_6L & _WRT4_OFF_6L & _WRT5_OFF_6L & _WRT6_OFF_6L & _WRT7_OFF_6L 
-	__CONFIG	_CONFIG6H, _WRTC_OFF_6H & _WRTB_OFF_6H & _WRTD_OFF_6H
-	__CONFIG	_CONFIG7L, _EBTR0_OFF_7L & _EBTR1_OFF_7L & _EBTR2_OFF_7L & _EBTR3_OFF_7L &  _EBTR4_OFF_7L & _EBTR5_OFF_7L & _EBTR6_OFF_7L & _EBTR7_OFF_7L 
-	__CONFIG	_CONFIG7H, _EBTRB_OFF_7H
+;	__CONFIG	_CONFIG1H, _OSCS_OFF_1H & _ECIO_OSC_1H		
+;	__CONFIG	_CONFIG2L, _BOR_OFF_2L & _BORV_20_2L & _PWRT_OFF_2L
+;	__CONFIG	_CONFIG2H, _WDT_OFF_2H & _WDTPS_128_2H
+;	__CONFIG	_CONFIG3L, _XMC_MODE_3L
+;	__CONFIG	_CONFIG3H, _CCP2MX_ON_3H
+;	__CONFIG	_CONFIG4L, _STVR_OFF_4L & _LVP_OFF_4L & _DEBUG_ON_4L
+;	__CONFIG	_CONFIG5L, _CP0_OFF_5L & _CP1_OFF_5L & _CP2_OFF_5L & _CP3_OFF_5L & _CP4_OFF_5L & _CP5_OFF_5L & _CP6_OFF_5L & _CP7_OFF_5L 
+;	__CONFIG	_CONFIG5H, _CPB_OFF_5H & _CPD_OFF_5H
+;	__CONFIG	_CONFIG6L, _WRT0_OFF_6L & _WRT1_OFF_6L & _WRT2_OFF_6L & _WRT3_OFF_6L & _WRT4_OFF_6L & _WRT5_OFF_6L & _WRT6_OFF_6L & _WRT7_OFF_6L 
+;	__CONFIG	_CONFIG6H, _WRTC_OFF_6H & _WRTB_OFF_6H & _WRTD_OFF_6H
+;	__CONFIG	_CONFIG7L, _EBTR0_OFF_7L & _EBTR1_OFF_7L & _EBTR2_OFF_7L & _EBTR3_OFF_7L &  _EBTR4_OFF_7L & _EBTR5_OFF_7L & _EBTR6_OFF_7L & _EBTR7_OFF_7L 
+;	__CONFIG	_CONFIG7H, _EBTRB_OFF_7H
+
+	;; This is the new way to define the configuration bits for 18F devices
+	CONFIG	OSCS = OFF, OSC = ECIO		
+	CONFIG	BOR = OFF, BORV = 25, PWRT = OFF
+	CONFIG	WDT = OFF, WDTPS = 128
+	CONFIG	MODE = EM
+	CONFIG	CCP2MUX = ON
+	CONFIG	STVR = OFF, LVP = OFF, DEBUG = OFF
+	CONFIG	CP0 = OFF, CP1 = OFF, CP2 = OFF, CP3 = OFF, CP4 = OFF, CP5 = OFF, CP6 = OFF, CP7 = OFF 
+	CONFIG	CPB = OFF, CPD = OFF
+	CONFIG	WRT0 = OFF, WRT1 = OFF, WRT2 = OFF, WRT3 = OFF,  WRT4 = OFF,  WRT5 = OFF, WRT6 = OFF, WRT7 = OFF 
+	CONFIG	WRTC = OFF, WRTB = OFF, WRTD = OFF
+	CONFIG	EBTR0 = OFF, EBTR1 = OFF, EBTR2 = OFF, EBTR3 = OFF,  EBTR4 = OFF, EBTR5 = OFF, EBTR6 = OFF, EBTR7 = OFF 
+	CONFIG	EBTRB = OFF
 
 	CBLOCK 0x000
 	ctr			; 0x000 counter used for loops
@@ -80,21 +96,21 @@
 	CAN_msg_data6
 	CAN_msg_data7
 	tray_posn				; 0x02D stores 3-bit tray position indicator which 
-							; serves as CAN address.
-							; 3 bits are left-justified
+						; serves as CAN address.
+						; 3 bits are left-justified
 	flash_addrU				; 0x02E flash memory address upper bits
 	flash_addrH				; 0x02F flash memory address high bits
 	flash_addrL				; 0x030 flash memory address low bits
 	flash_dataH				; 0x031 flash data high bits
 	flash_dataL				; 0x032 flash data low bits
-	PLD_word:		0x04	; 0x033 - 0x036 TDC data word from PLD
+	PLD_word:       0x04	; 0x033 - 0x036 TDC data word from PLD
 	config1_data: 	0x51	; 0x037 - 0x087
 	config2_data: 	0x51	; 0x088 - 0x0D8
 	config3_data:	0x51	; 0x0D9 - 0x129
 	config4_data:	0x51	; 0x12A - 0x17A
 	config_temp:	0x51	; 0x17B - 0x1CB cache for config data
 	new_prog_data:	0x40	; 0x1CC - 0x20B	64 bytes of new program data
-							; (temporary cache before writing to FLASH)
+				; (temporary cache before writing to FLASH)
 	endc
 
 	constant	mask0 = 0x01
@@ -198,14 +214,14 @@ START
 ;**********************************************************************
 	
 	nop
-	setf	prev_desc			; this is an illegal prev_desc.  Forces a START to be issued first
+	setf	prev_desc		; this is an illegal prev_desc.  Forces a START to be issued first
 	setf	prev_reprog_desc
 	setf	prev_reprog_addrH
 	setf	prev_reprog_addrL	; all illegal values to ensure START is issued first.
 
 	movlw	0x97
-	movwf	ctr					; This block gives DAC the value 0x597
-	movlw	0x45				; which is 30mV (standard) threshold
+	movwf	ctr			; This block gives DAC the value 0x597
+	movlw	0x45			; which is 30mV (standard) threshold
 	movwf	write_to
 	call	new_DAC_value
 		
@@ -245,18 +261,18 @@ check_CANbus_JTAG
 
 ;------------------------------------------------------------------------------
 
-	#include "MCU_config.asm"			; configuration routines for MCU
+	#include "MCU_config.asm"		; configuration routines for MCU
 	#include "SPI_functions.asm"		; SPI functions for DAC and CAN controller access	
 	#include "CAN_functions.asm"		; sending/receiving CAN functions	
 	#include "CAN_HLP_functions.asm"	; functions implementing CAN HLP
 	#include "JTAG_functions.asm"		; IRScan, DRScan
 	#include "TDC_functions.asm"		; MCU JTAG instructions "lock" "get_data" etc
-	#include "housekeeping.asm"			; parity, stall, etc
+	#include "housekeeping.asm"		; parity, stall, etc
 	#include "PLD_functions.asm"		; PLD interface
 	#IF CODE_BASE == 0x0000
-	#include "reprogram.asm"			; Program memory reprogramming code
+	#include "reprogram.asm"		; Program memory reprogramming code
 	#ENDIF
-	#include "TDC_config.asm"			; TDC configuration constants
+	#include "TDC_config.asm"		; TDC configuration constants
 
 ;******************************************************************************
 
