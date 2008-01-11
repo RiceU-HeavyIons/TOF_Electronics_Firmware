ed294efba6b1ff36cddac1776ffaf1fb86063480
J Schambach <jschamba@physics.utexas.edu>
2008-01-11 17:46:12 +0000

    8 bit Data from Serdes FPGA is taken from rising and falling edge of the 80 MHz clock that is also transmitted by the Serdes FPGA and de-multiplexed into two 8 bit streams. These two 8 bits streams and the latch signal are input into a dc FIFO to sync them to the local 80 MHz clock. The latch signal out of the FIFO is used to generate a delayed latch signal. The 16 bit data out from the FIFO is shifted into one 32 bit word The SerdesReader state machine controls when all of this is done and outputs the appropriate data and latch signals for a FIFO to latch outside of this component.

--- THUB/master_fpga/serdes_reader.vhd
+++ THUB/master_fpga/serdes_reader.vhd
@@ -1,4 +1,4 @@
--- $Id: serdes_reader.vhd,v 1.6 2008-01-07 15:12:40 jschamba Exp $
+-- $Id: serdes_reader.vhd,v 1.7 2008-01-11 17:46:12 jschamba Exp $
 -------------------------------------------------------------------------------
 -- Title      : Serdes Reader
 -- Project    : 
@@ -7,7 +7,7 @@
 -- Author     : 
 -- Company    : 
 -- Created    : 2007-11-21
--- Last update: 2008-01-04
+-- Last update: 2008-01-11
 -- Platform   : 
 -- Standard   : VHDL'93/02
 -------------------------------------------------------------------------------
@@ -56,8 +56,26 @@ END ENTITY serdes_reader;
 
 ARCHITECTURE a OF serdes_reader IS
 
+  COMPONENT ddio_in IS
+    PORT (
+      datain    : IN  std_logic_vector (7 DOWNTO 0);
+      inclock   : IN  std_logic;
+      dataout_h : OUT std_logic_vector (7 DOWNTO 0);
+      dataout_l : OUT std_logic_vector (7 DOWNTO 0));
+  END COMPONENT ddio_in;
+
   SIGNAL s_outdata   : std_logic_vector (31 DOWNTO 0);
   SIGNAL s_wrreq_out : std_logic;
+
+  SIGNAL block_end      : boolean;
+  SIGNAL sl_areset_n    : std_logic;
+  SIGNAL s_slatch       : std_logic;
+  SIGNAL s_prelatch     : std_logic;
+  SIGNAL s_ddio_outh    : std_logic_vector (7 DOWNTO 0);
+  SIGNAL s_ddio_outl    : std_logic_vector (7 DOWNTO 0);
+  SIGNAL shift_areset_n : std_logic;
+  SIGNAL s_shiftout     : std_logic_vector (31 DOWNTO 0);
+  signal sync_q         : std_logic_vector (16 downto 0);
 
   TYPE TState_type IS (
     SWaitTrig,
@@ -76,33 +94,93 @@ BEGIN  -- ARCHITECTURE a
   wrreq_out <= s_wrreq_out;
   outdata   <= s_outdata;
 
+  -- first decode both edges of the incoming data stream with the "double data
+  -- rate" component. clock is taken from the Serdes input pins
+  ddio_in_inst : ddio_in PORT MAP (
+    datain    => indata(7 DOWNTO 0),
+    inclock   => indata(15),
+    dataout_h => s_ddio_outh,
+    dataout_l => s_ddio_outl);
 
-  shifter : PROCESS (clk80mhz, areset_n) IS
-    VARIABLE ctr       : integer RANGE 0 TO 2    := 0;
-    VARIABLE delayCtr  : integer RANGE 0 TO 2047 := 0;
-    VARIABLE block_end : boolean;
+  -- now synchronize the 2 decoded 8bit streams and the latch signal with a
+  -- dual-clock FIFO
+  syncfifo : dcfifo
+    generic map (
+      intended_device_family => "Cyclone II",
+      lpm_numwords           => 8,
+      lpm_showahead          => "OFF",
+      lpm_type               => "dcfifo",
+      lpm_width              => 17,
+      lpm_widthu             => 3,
+      overflow_checking      => "ON",
+      rdsync_delaypipe       => 4,
+      underflow_checking     => "ON",
+      use_eab                => "ON",
+      wrsync_delaypipe       => 4
+      )
+    port map (
+      wrclk             => indata(15),
+      wrreq             => '1',
+      rdclk             => clk80mhz,
+      rdreq             => '1',
+      data(16)          => indata(8),
+      data(15 downto 8) => s_ddio_outl,
+      data(7 downto 0)  => s_ddio_outh,
+      q                 => sync_q
+      );
+
+  -- create a delayed latch signal
+  serdesLatch : PROCESS (clk80mhz, sl_areset_n) IS
+  BEGIN
+    IF sl_areset_n = '0' THEN           -- asynchronous reset (active low)
+      s_slatch   <= '0';
+      s_prelatch <= '0';
+    ELSIF clk80mhz'event AND clk80mhz = '0' THEN  -- falling clock edge
+      s_slatch   <= s_prelatch;
+      s_prelatch <= sync_q(16);
+    END IF;
+  END PROCESS serdesLatch;
+
+  -- use a shift register to make a 32 bit word out of the 16 bit stream
+  shifter : PROCESS (clk80mhz, shift_areset_n) IS
+  BEGIN
+    IF shift_areset_n = '0' THEN        -- asynchronous reset (active low)
+      s_shiftout <= (OTHERS => '0');
+      
+    ELSIF clk80mhz'event AND clk80mhz = '0' THEN  -- falling clock edge
+      s_shiftout(31 DOWNTO 16) <= s_shiftout(15 DOWNTO 0);
+      s_shiftout(15 DOWNTO 0)  <= sync_q(15 DOWNTO 0);
+    END IF;
+  END PROCESS shifter;
+
+
+  -- use a state machine to control the Serdes read process
+  rdoutControl : PROCESS (clk80mhz, areset_n) IS
+    VARIABLE delayCtr : integer RANGE 0 TO 2047 := 0;
   BEGIN
     IF areset_n = '0' THEN              -- asynchronous reset (active low)
-      s_outdata     <= (OTHERS => '0');
-      ctr           := 0;
-      rdreq_out     <= '0';
-      block_end     := false;
-      TState        <= SWaitTrig;
-      busy          <= '1';             -- default is "not busy"
-      trgFifo_rdreq <= '0';
+      s_outdata      <= (OTHERS => '0');
+      rdreq_out      <= '0';
+      block_end      <= false;
+      TState         <= SWaitTrig;
+      busy           <= '1';            -- default is "not busy"
+      trgFifo_rdreq  <= '0';
+      sl_areset_n    <= '0';
+      shift_areset_n <= '0';
       
     ELSIF clk80mhz'event AND clk80mhz = '1' THEN  -- rising clock edge
-      s_wrreq_out   <= '0';
-      rdreq_out     <= '0';
-      busy          <= '0';                       -- default is "busy"
-      trgFifo_rdreq <= '0';
+      s_wrreq_out    <= '0';
+      rdreq_out      <= '0';
+      busy           <= '0';                      -- default is "busy"
+      trgFifo_rdreq  <= '0';
+      sl_areset_n    <= '0';
+      shift_areset_n <= '1';
 
       CASE TState IS
 
         -- wait for trigger
         WHEN SWaitTrig =>
           busy <= '1';                  -- "not busy" until trigger
-          ctr  := 0;
           IF evt_trg = '1' THEN
             TState <= SFifoChk;
           END IF;
@@ -117,7 +195,7 @@ BEGIN  -- ARCHITECTURE a
         WHEN STagWrd =>
           s_outdata   <= X"DEADFACE";
           s_wrreq_out <= '1';
-          
+
           TState <= SRdSerA;
 
           -- deserialize the 16bit input stream into 32bit output
@@ -126,29 +204,22 @@ BEGIN  -- ARCHITECTURE a
           -- Stop when we see a word starting  with "0xE0" (the
           -- separator word generated by the 2nd cable on TCPU)
         WHEN SRdSerA =>
-          rdreq_out <= '1';
+          rdreq_out   <= '1';
+          sl_areset_n <= '1';
 
-          IF (indata(15 DOWNTO 8) = X"E0") AND (ctr = 0) THEN
-            block_end := true;
+          IF s_shiftout(31 DOWNTO 24) = X"E0" THEN
+            block_end <= true;
           END IF;
 
-          IF fifo_empty = '0' THEN
-            ctr                     := ctr + 1;
-            s_outdata(15 DOWNTO 0)  <= indata;
-            s_outdata(31 DOWNTO 16) <= s_outdata(15 DOWNTO 0);
-          END IF;
-
-          IF ctr = 2 THEN
-            IF block_end THEN
-              block_end := false;
-              TState    <= SDelay;
-            END IF;
-
-            s_wrreq_out <= '1';
-            ctr         := 0;
-          END IF;
+          s_outdata   <= s_shiftout;
+          s_wrreq_out <= s_slatch;
 
           delayCtr := 0;
+
+          IF block_end AND (s_slatch = '1') THEN
+            block_end <= false;
+            TState    <= SDelay;
+          END IF;
 
           -- delay for a while (~13 us)
         WHEN SDelay =>
@@ -183,6 +254,6 @@ BEGIN  -- ARCHITECTURE a
       END CASE;
       
     END IF;
-  END PROCESS shifter;
+  END PROCESS rdoutControl;
   
 END ARCHITECTURE a;
