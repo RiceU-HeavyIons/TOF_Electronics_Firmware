64da5bc0e10cf63d0a3bc85516361468b9d30dcd
J Schambach <jschamba@physics.utexas.edu>
2008-08-13 17:54:25 +0000

    This commit was manufactured by cvs2svn to create tag 'BNL_20080809'.

--- TDIG/PLD/mux_2x32_registered_inst.vhd
+++
@@ -1,9 +0,0 @@
-mux_2x32_registered_inst : mux_2x32_registered PORT MAP (
-		clock	 => clock_sig,
-		aclr	 => aclr_sig,
-		clken	 => clken_sig,
-		data1x	 => data1x_sig,
-		data0x	 => data0x_sig,
-		sel	 => sel_sig,
-		result	 => result_sig
-	);
