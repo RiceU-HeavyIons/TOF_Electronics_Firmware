20543087f0abdfb0e7d02e4c5c8116a5bb56b373
J Schambach <jschamba@physics.utexas.edu>
2009-03-13 18:56:27 +0000

    Bunch reset needs the phase between 40 and 20MHz clock transmitted to adjust correct bunch reset time in the TCPU, just like trigger Put the phase into txd[11] (trigger is in bit 12), since this is the first bit not used yet

--- THUB/serdes_fpga/smif.vhd
+++ THUB/serdes_fpga/smif.vhd
@@ -1,4 +1,4 @@
--- $Id: smif.vhd,v 1.5 2008-05-14 19:10:27 jschamba Exp $
+-- $Id: smif.vhd,v 1.6 2009-03-13 18:56:27 jschamba Exp $
 -------------------------------------------------------------------------------
 -- Title      : serdes-master-if
 -- Project    : SERDES_FPGA
@@ -7,7 +7,7 @@
 -- Author     : J. Schambach
 -- Company    : 
 -- Created    : 2007-05-14
--- Last update: 2008-05-12
+-- Last update: 2009-03-13
 -- Platform   : 
 -- Standard   : VHDL'93
 -------------------------------------------------------------------------------
@@ -53,10 +53,10 @@ END smif;
 
 ARCHITECTURE a OF smif IS
 
-  CONSTANT GEO_ID_CH0 : std_logic_vector := CONV_STD_LOGIC_VECTOR(121, 7);  -- tray 121
-  CONSTANT GEO_ID_CH1 : std_logic_vector := CONV_STD_LOGIC_VECTOR( 77, 7);  -- tray 77
-  CONSTANT GEO_ID_CH2 : std_logic_vector := CONV_STD_LOGIC_VECTOR( 78, 7);  -- tray 78
-  CONSTANT GEO_ID_CH3 : std_logic_vector := CONV_STD_LOGIC_VECTOR( 79, 7);  -- tray 79
+  CONSTANT GEO_ID_CH0 : std_logic_vector := CONV_STD_LOGIC_VECTOR(124, 7);
+  CONSTANT GEO_ID_CH1 : std_logic_vector := CONV_STD_LOGIC_VECTOR(125, 7);
+  CONSTANT GEO_ID_CH2 : std_logic_vector := CONV_STD_LOGIC_VECTOR(126, 7);
+  CONSTANT GEO_ID_CH3 : std_logic_vector := CONV_STD_LOGIC_VECTOR(127, 7);
 
   TYPE State_type IS (State0, State1, State2, State3, State4, State7);
   SIGNAL state        : State_type;
@@ -85,31 +85,31 @@ BEGIN
       geo_id2      <= GEO_ID_CH2;
       geo_id3      <= GEO_ID_CH3;
       
-    ELSIF clk40mhz'event AND clk40mhz = '1' THEN  -- leading clock edge
+    ELSIF rising_edge(clk40mhz) THEN
       trigger <= '0';
-      
+
       CASE state IS
         -- ************* Waiting... ********************************
         WHEN State0 =>
           -- wait for a valid "data_type" pattern on rising clock edge
           -- latch the input data into signal s_datain
           -- set the SERDES data lines all to '0'
-          s_serdes_out <= (OTHERS => '0');      -- normally all 0's
-          s_datain     <= datain;               -- latch datain
-          IF data_type = "0011" THEN            -- goto "send trigger data"
+          s_serdes_out <= (OTHERS => '0');  -- normally all 0's
+          s_datain     <= datain;           -- latch datain
+          IF data_type = "0011" THEN        -- goto "send trigger data"
             state <= State1;
-          ELSIF data_type = "0101" THEN         -- goto "send bunch reset"
+          ELSIF data_type = "0101" THEN     -- goto "send bunch reset"
             state <= State2;
-          ELSIF data_type = "1001" THEN         -- goto "send tcpu reset"
+          ELSIF data_type = "1001" THEN     -- goto "send tcpu reset"
             state <= State3;
-          ELSIF data_type = "1010" THEN         -- goto "load register"
+          ELSIF data_type = "1010" THEN     -- goto "load register"
             state <= State4;
           END IF;
 
-        -- *************** trigger **********************************
+          -- *************** trigger **********************************
         WHEN State1 =>
           trigger <= '1';
-          
+
           -- latch the 20MHz clock phase
           -- calculate the parity
           -- set SERDES data lines to appropriate values
@@ -119,22 +119,26 @@ BEGIN
           s_serdes_out(17)          <= '1';
           state                     <= State7;
 
-        -- *************** bunch reset ******************************
+          -- *************** bunch reset ******************************
         WHEN State2 =>
-          s_serdes_out <= (16 => '1', 13 => '1', OTHERS => '0');
-          state        <= State7;
+          -- serdes_out(17:16) = "01", serdes_out(15:12) = "0010"
+          s_serdes_out(10 DOWNTO 0)  <= (OTHERS => '0');
+          s_serdes_out(11)           <= clk20mhz;  -- clock phase
+          s_serdes_out(17 DOWNTO 12) <= "010010";
+          state                      <= State7;
 
-        -- *************** tcpu reset *******************************
+          -- *************** tcpu reset *******************************
         WHEN State3 =>
+          -- serdes_out(17:16) = "01", serdes_out(15:12) = "0011"
           s_serdes_out <= (16 => '1', 13 => '1', 12 => '1', OTHERS => '0');
           state        <= State7;
 
-        -- *************** load register ****************************
+          -- *************** load register ****************************
         WHEN State4 =>
           -- latch the lowest 8 bits of the incoming data into
           -- "serdes_reg" (which has implied memory)
           IF s_datain(7) = '0' THEN
-            serdes_reg <= s_datain(6 DOWNTO 0);     -- implied memory
+            serdes_reg <= s_datain(6 DOWNTO 0);  -- implied latch
           ELSE
             CASE reg_no IS
               WHEN 0 =>
@@ -143,19 +147,19 @@ BEGIN
                 geo_id1 <= s_datain(6 DOWNTO 0);
               WHEN 2 =>
                 geo_id2 <= s_datain(6 DOWNTO 0);
-              WHEN OTHERS => 
+              WHEN OTHERS =>
                 geo_id3 <= s_datain(6 DOWNTO 0);
             END CASE;
             reg_no := reg_no + 1;
           END IF;
 
-          state      <= State7;
-          
-        -- *************** wait state *******************************
+          state <= State7;
+
+          -- *************** wait state *******************************
         WHEN State7 =>
           state <= State0;
-          
-        -- *************** invalid state ****************************
+
+          -- *************** invalid state ****************************
         WHEN OTHERS =>                  -- shouldn't happen
           state <= State0;
       END CASE;
