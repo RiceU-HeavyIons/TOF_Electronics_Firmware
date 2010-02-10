c04936d5da5f8b9f0d252cde0a1de980e5ec2b63
J Schambach <jschamba@physics.utexas.edu>
2010-02-10 17:12:34 +0000

    moved the following functions here: get_MCU_pm WriteMem WriteLatch

--- TDIGrev/MCU/rtspApi.s
+++ TDIGrev/MCU/rtspApi.s
@@ -63,8 +63,41 @@
 	.global _flashPageErase
 	.global _flashPageWrite
 	.global _flashPageModify
+    .global _get_MCU_pm
+    .global _WriteMem,_WriteLatch
 
 	.section .text
+
+;***************************************************************	
+_get_MCU_pm: ;W0=TBLPAG,W1=Wn - data in W1:W0
+	mov	W0,TBLPAG	
+	tblrdl [W1],W0
+	tblrdh [W1],W1
+	
+	return
+
+;***************************************************************		
+_WriteMem:	;W0=NVMCON - no return values
+	mov	W0,NVMCON
+	mov	#0x55,W0	;Unlock sequence - interrupts need to be off
+	mov	W0,NVMKEY
+	mov	#0xAA,W0
+	mov	W0,NVMKEY
+	bset NVMCON,#WR
+	nop				;Required
+	nop
+1:	btsc NVMCON,#WR	;Wait for write end
+	bra 1b
+	
+	return
+
+;***************************************************************	
+_WriteLatch: ;W0=TBLPAG,W1=Wn,W2=WordHi,W3=WordLo - no return values
+	mov	W0,TBLPAG	
+	tblwtl W3,[W1]
+	tblwth W2,[W1]
+	
+	return
 
 /******************************************************************************
   Flash Page Read 
