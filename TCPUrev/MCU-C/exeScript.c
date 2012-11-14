767f8e63281f53d42eb5d4543b18aad0ef3349f8
J Schambach <jschamba@physics.utexas.edu>
2012-11-14 19:15:36 +0000

    Added Set thresholds on all TDIGs to exeScripts

--- TCPUrev/MCU-C/exeScript.c
+++ TCPUrev/MCU-C/exeScript.c
@@ -66,7 +66,7 @@ int __attribute__((__section__(".script_buffer"))) exeScript(unsigned int board_
 		j = 0xfff;
 	    do {--j;} while ((C1TR01CONbits.TXREQ0==1) && (j != 0));
 		if (j == 0) return -1; // timed out, don't continue
-		msg_id = (unsigned int)(i<<6) | C_WRITE; // TDIG 0x11
+		msg_id = (unsigned int)(i<<6) | C_WRITE; // TDIG i write
     	ecan1msgBuf[0][0] = msg_id;  // extended ID =0, no remote xmit
     	C1TR01CONbits.TXREQ0=1; // Mark message buffer ready-for-transmit on CAN#1
 		j = 0x5fff;	do {--j;} while (j != 0); // idle a little to pace the messages
@@ -126,15 +126,70 @@ int __attribute__((__section__(".script_buffer"))) exeScript(unsigned int board_
 		}
 	}
 
+	// ****************************************************************
+	// 2) set threshold on each TDIG to 1500mV
+	// threshold = 1500.0;
+	// val = (unsigned int)((threshold * 4095.0 / 3300.0 + 0.5) = 0x746
+	// set threshold command: m s 0xXX2 3 0x08 val&0xff (val>>8)&0xff
+	// ****************************************************************
+    ecan1msgBuf[0][1] = 0;
+    ecan1msgBuf[0][2] = 3; // length
+	ecan1msgBuf[0][3] = 0x4608; // data[1,0]
+	ecan1msgBuf[0][4] = 0x0007; // data[3,2]
+
+	numRcvd = 0; // keep track of number of responses
+	for (i=0x10; i<0x18; i++) { 
+	    // wait for transmit on CAN1 to complete or timeout
+		j = 0xfff;
+	    do {--j;} while ((C1TR01CONbits.TXREQ0==1) && (j != 0));
+		if (j == 0) return -1; // timed out, don't continue
+		msg_id = (unsigned int)(i<<6) | C_WRITE; // TDIG i write
+    	ecan1msgBuf[0][0] = msg_id;  // extended ID =0, no remote xmit
+    	C1TR01CONbits.TXREQ0=1; // Mark message buffer ready-for-transmit on CAN#1
+		j = 0x5fff;	do {--j;} while (j != 0); // idle a little to pace the messages
+		// check if we already got a response
+		if (C1RXFUL1bits.RXFUL2==1) {
+    	    C1RXFUL1bits.RXFUL2 = 0;        // mark CAN1 Receive-Buffer 2 OK to re-use
+			numRcvd++;
+		}
+	}
+
+	// check for responses, expect 8, 
+	// but check more than necessary to account for time of responses to come back
+	for (i=1; i<12; i++) {
+		// once we received 8 responses, we are finished:
+		if (numRcvd == 8) break;
+		// now check for response with timeout
+		j = 0xffff;
+	    do {--j;} while ((C1RXFUL1bits.RXFUL2==0) && (j != 0));
+		if (j != 0) {
+    	    C1RXFUL1bits.RXFUL2 = 0;        // mark CAN1 Receive-Buffer 2 OK to re-use
+			numRcvd++;
+		}
+		else {
+			// no message received within timeout, send alert on CAN2
+			msg_id = (unsigned int)(0x20<<6) | C_ALERT;
+    		ecan2msgBuf[0][0] = msg_id;  // extended ID =0, no remote xmit
+    		ecan2msgBuf[0][1] = 0;
+    		ecan2msgBuf[0][2] = 4; // length
+			ecan2msgBuf[0][3] = i; // data[1,0]
+			ecan2msgBuf[0][4] = numRcvd; // data[3,2]
+			j = 0xffff;
+	    	do {--j;} while ((C1TR01CONbits.TXREQ0==1) && (j != 0));
+			if (j != 0)
+        		C2TR01CONbits.TXREQ0=1; // Mark message buffer ready-for-transmit on CAN2
+		}
+	}
+
 	// ****************************************************
-	// 2) write multiplicity gate value to FPGA address 0x8
+	// 3) write multiplicity gate value to FPGA address 0x8
 	// ****************************************************
 	addr = 8;
 	val = 2; //multiplicity gate value
 	write_FPGA(addr, val);
 
 	// ****************************************************
-	// 3) reset SerDes Link
+	// 4) reset SerDes Link
 	// ****************************************************
 	addr = 2;
 	val = 0; // FPGA Reg2 = 0
